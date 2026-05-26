41da5013e8bf32d97704e80ded5a7fae03f5bd99
Yuh <huypham3188@gmail.com>
2026-05-26 22:59:06 +0700

    add config header for ngrok

--- src/main/java/com/vpgh/dms/config/SecurityConfiguration.java
+++ src/main/java/com/vpgh/dms/config/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry", "ngrok-skip-browser-warning"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
